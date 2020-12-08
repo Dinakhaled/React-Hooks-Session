7b1b2df0647b55669ccadab468a56ad88bf73385
DinaKhaled <dina.elghndour@gmail.com>
2020-12-08 14:10:27 +0200

    Render button in home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Button from "../../components/Button";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container my-5">
+      <h1>0</h1>
+      <Button />
+    </div>
+  );
 };
 
 export default Home;
